c7c8e7f16b5b552e08d565c41de30a8d073c694d
pradpant <ppant85@gmail.com>
2020-11-09 14:21:14 +0530

    commited full name

--- src/trialGithub/MyPerName.java
+++ src/trialGithub/MyPerName.java
@@ -5,6 +5,7 @@ public class MyPerName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Pradeep");
+		System.out.println("Fulle name is Pradeep Pant");
 	}
 
 }
